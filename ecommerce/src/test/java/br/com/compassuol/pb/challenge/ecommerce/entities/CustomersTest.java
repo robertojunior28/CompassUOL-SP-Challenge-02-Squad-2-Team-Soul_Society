229225ca701cf926e7cf1f5a26209ddd5501e25f
Lucas Tosta <lucasvalerio30@gmail.com>
2023-06-07 15:00:47 -0300

    assert implemetations

--- ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/entities/CustomersTest.java
+++ ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/entities/CustomersTest.java
@@ -2,6 +2,8 @@ package br.com.compassuol.pb.challenge.ecommerce.entities;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
  @DataJpaTest
  class CustomersTest {
@@ -14,6 +16,16 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
          customer.setEmail("estagio@compass.com");
          customer.setActive(true);
 
+         String name = customer.getName();
+         String cpf = customer.getCpf();
+         String email = customer.getEmail();
+         boolean active = customer.getActive();
+
          System.out.println(customer.getCustomerId());
+
+         assertEquals("Customer A", name);
+         assertEquals("000.000.000-00", cpf);
+         assertEquals("estagio@compass.com", email);
+         assertTrue(active);
      }
 }
