8809a94436d9287710c977905cdcfc4dcefee9ef
Lucas Tosta <lucasvalerio30@gmail.com>
2023-06-07 13:28:29 -0300

    atualized

--- ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/controller/CustomerResource.java
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/controller/CustomerResource.java
@@ -1,14 +1,38 @@
 package br.com.compassuol.pb.challenge.ecommerce.controller;
 
+import br.com.compassuol.pb.challenge.ecommerce.entities.Customer;
+import br.com.compassuol.pb.challenge.ecommerce.entities.Product;
 import br.com.compassuol.pb.challenge.ecommerce.services.CustomerDaoService;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.compassuol.pb.challenge.ecommerce.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-public class CustomerResource {
+public class CustomerController {
 
-    private final CustomerDaoService service;
+    @Autowired
+    private CustomerRepository customerRepository;
 
-    public CustomerResource(CustomerDaoService service) {this.service = service; }
+    @GetMapping("/v1/products/{id}")
+    public Product retrieveProductById(@PathVariable Integer id){
+        return service.findById(id);
+    }
 
+    @PostMapping("v1/customers")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Customer createCustomer(@RequestBody Customer customer){
+        return customerRepository.save(customer);
+    }
+
+    @PutMapping("v1/customers/{id}")
+    public Customer updateCustomer(@PathVariable Integer id, @RequestBody Customer customer ){
+        var customerOptional = customerRepository.findById(id);
+        if(customerOptional.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return customerRepository.save(customer);
+    }
 
 }
--- ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/entities/Product.java
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/entities/Product.java
@@ -4,9 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Entity
