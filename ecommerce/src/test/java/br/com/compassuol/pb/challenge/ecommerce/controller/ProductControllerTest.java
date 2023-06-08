9af9dc0dd5daeec42e4d3cb3dda3b5a18ffe60e8
Lucas Tosta <lucasvalerio30@gmail.com>
2023-06-08 15:32:12 -0300

    Test class created

---
+++ ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/controller/ProductControllerTest.java
@@ -0,0 +1,18 @@
+package br.com.compassuol.pb.challenge.ecommerce.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProductControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testValidProductController() {
+
+    }
+}
---
+++ ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/services/ProductDaoServiceTest.java
@@ -0,0 +1,17 @@
+package br.com.compassuol.pb.challenge.ecommerce.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductDaoServiceTest {
+
+    @Autowired
+    private ProductDaoService productDaoService;
+
+    @Test
+    public void testProductDaoService() {
+
+    }
+}
