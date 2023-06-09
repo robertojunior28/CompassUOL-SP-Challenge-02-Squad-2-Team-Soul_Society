cb53a126f3a27a6e1bd9a076a178fd9549dcc6f4
Lucas Tosta <lucasvalerio30@gmail.com>
2023-06-09 17:35:34 -0300

    basic ordertest

--- ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/entities/OrderTest.java
+++ ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/entities/OrderTest.java
@@ -10,20 +10,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DataJpaTest
 class OrderTest {
 
+    Order order = new Order();
+
     @Test
-    public void testValidOrder(){
-        Order order = new Order();
-        order.setStatus("Status ");
+    void setId(){
+        order.setId(2);
+        assertEquals(2, order.getId());
+    }
+
+    @Test
+    void setStatus() {
+        order.setStatus();
+        assertEquals(, order.getStatus());
+    }
+
+    @Test
+    void setCustomer() {
         order.setCustomer();
-        order.setDate();
+        assertEquals(, order.getCustomer());
+    }
 
-        String status = order.getStatus();
-        String customer = order.getCustomer();
-
-        System.out.println(order.getId());
-
-        assertEquals("Status A", status);
-        assertEquals("Customer A", customer);
-
+    @Test
+    void setDate(){
+        order.setDate(new Date());
+        assertEquals(new Date(), order.getDate());
     }
 }
