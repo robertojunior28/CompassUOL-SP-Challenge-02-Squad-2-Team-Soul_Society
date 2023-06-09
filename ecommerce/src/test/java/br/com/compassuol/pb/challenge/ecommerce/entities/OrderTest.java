d0a994dc4602cc5393da7d35a1d5d492ca3ed001
Lucas Tosta <lucasvalerio30@gmail.com>
2023-06-09 17:15:20 -0300

    Tests and errors

--- ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/entities/OrderTest.java
+++ ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/entities/OrderTest.java
@@ -2,6 +2,9 @@ package br.com.compassuol.pb.challenge.ecommerce.entities;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
@@ -10,9 +13,9 @@ class OrderTest {
     @Test
     public void testValidOrder(){
         Order order = new Order();
-        order.setStatus("Status A");
-        order.setCustomer("Customer A");
-        order.setDate("2023-06-08");
+        order.setStatus("Status ");
+        order.setCustomer();
+        order.setDate();
 
         String status = order.getStatus();
         String customer = order.getCustomer();
@@ -20,6 +23,7 @@ class OrderTest {
         System.out.println(order.getId());
 
         assertEquals("Status A", status);
+        assertEquals("Customer A", customer);
 
     }
 }
--- ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/entities/PaymentTest.java
+++ ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/entities/PaymentTest.java
@@ -2,12 +2,26 @@ package br.com.compassuol.pb.challenge.ecommerce.entities;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 class PaymentTest {
 
     @Test
-    public void testValidPayment() {
+    public void testValidPayment(){
+        Payment payment = new Payment();
+        payment.setPaymentMethod("PIX");
+        payment.setPaymentDate("2023-06-09");
+        payment.setOrder("Order A");
 
+        String paymentMethod = String.valueOf(payment.getPaymentMethod());
+        String paymentDate = String.valueOf(payment.getPaymentDate());
+        String order = payment.getOrder().toString();
+
+        System.out.println(payment.getPaymentId());
+
+        assertEquals("PIX", paymentMethod);
+        assertEquals("2023-06-09", paymentDate);
+        assertEquals("estagio@compass.com", order);
     }
 }
