40d8234b5fefdad2feab6b560d62793ab303ef21
Jonathan Julião <jon.m.juliao@gmail.com>
2023-06-10 16:49:06 -0300

    Some simple updates

--- ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/services/ProductServiceTest.java
+++ ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/services/ProductServiceTest.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class ProductDaoServiceTest {
+class ProductServiceTest {
 
     @Mock
     private ProductRepository productRepo;
