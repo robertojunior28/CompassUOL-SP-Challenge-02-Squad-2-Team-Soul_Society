c996e8120c5392725a899c5ecc0f5aabfaf5d892
NotUser <jmtorrichelli@gmail.com>
2023-06-12 17:19:34 -0300

    updated handler exception

--- ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/exceptions/ProductNotFoundException.java
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/exceptions/ProductNotFoundException.java
@@ -1,8 +1,5 @@
 package br.com.compassuol.pb.challenge.ecommerce.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class ProductNotFoundException extends RuntimeException{
     public ProductNotFoundException(Integer id) {
         super(String.format("Product with id %s not found", id));
